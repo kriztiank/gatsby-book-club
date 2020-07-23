e5641e0f2cbbc9a647d652cdd8726198441251f6
kriztiank <kriztiank@gmail.com>
2020-07-23 09:55:34 +0200

    first commit

---
+++ src/components/Firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDtFBXIZw_n3Di5FrtcoEq5Y3UvdV4tbmg",
+  authDomain: "gatsby-book-club-75ae4.firebaseapp.com",
+  databaseURL: "https://gatsby-book-club-75ae4.firebaseio.com",
+  projectId: "gatsby-book-club-75ae4",
+  storageBucket: "gatsby-book-club-75ae4.appspot.com",
+  messagingSenderId: "824858311981",
+  appId: "1:824858311981:web:0f77b06297c55f6e6867a4"
+};
+
+export default firebaseConfig;
